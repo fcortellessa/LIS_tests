984d9041133937318244e928ba51c4dc2233d898
Flavia Cortellessa <133102589+fcortellessa@users.noreply.github.com>
2024-06-10 14:18:21 +0200

    Update 2D_RRT.py
    grasping at given height works now

--- 2D_Puzzle/2D_RRT.py
+++ 2D_Puzzle/2D_RRT.py
@@ -43,7 +43,7 @@ def solve_maze_rrt(C: ry.Config, visual: bool, distance: float) -> ry._robotic.S
                 .setPosition([q[0], q[1], 0.0]) \
                 .setColor([1, 1, 1])
             ret.x[i] = [q[0], q[1], table_height]   # move path to table height 0.6 in z axis 
-            print(f'found path:{ret.x}')
+            # print(f'found path:{ret.x}')
             C.addFrame(f"marker_exec{i}") \
                 .setShape(ry.ST.marker, size=[.04]) \
                 .setPosition([q[0], q[1], table_height]) \
@@ -54,7 +54,7 @@ def solve_maze_rrt(C: ry.Config, visual: bool, distance: float) -> ry._robotic.S
     
     return ret
 
-def plan_path(q_pWorld, q_start, q_goal, table_height, scenario, box_size):
+def plan_path(q_pWorld, q_start, q_goal, table_height, scenario, box_size, hold_size: list):
     ry.params_add({'rrt/stepsize': .05})
     C = ry.Config()
     puzzle_world = pWorld.PuzzleWorld(C, None, q_pWorld, q_start, q_goal)
@@ -75,7 +75,8 @@ def plan_path(q_pWorld, q_start, q_goal, table_height, scenario, box_size):
     C.delFrame("moving_box")
     
     box_pos = C.getFrame("start").getRelativePosition()
-    box_pos[2] = 0.015
+    # box_pos[2] = 0.015  # why 0.015? because the box or the table is 0.03 high??
+    box_pos[2] = box_size[2]/2.0
     C.addFrame("box", "puzzle_world") \
         .setRelativePosition(box_pos) \
         .setShape(ry.ST.ssBox, size=box_size) \
@@ -85,11 +86,15 @@ def plan_path(q_pWorld, q_start, q_goal, table_height, scenario, box_size):
         .setMass(1.)
     if hold == True:
         C.addFrame("hold", "box") \
-            .setRelativePosition([0.0, 0.0, box_size[2]/2 + 0.025]) \
-            .setShape(ry.ST.ssBox, size=[.04, .04, .05, .001]) \
+            .setRelativePosition([0.0, 0.0, box_size[2]/2.0 + hold_size[2]/2.0]) \
+            .setShape(ry.ST.ssBox, size=hold_size) \
             .setColor([1, 1, .0]) \
             .setContact(1) \
             .setMass(1.)
+        C.addFrame("holdcenter", "hold") \
+            .setRelativePosition([0.0, 0.0, 0.0]) \
+            .setShape(ry.ST.marker, size=[0.1, 0.1, 0.1]) \
+            .setColor([1, 1, 1])
     C.view()
     return C, path
 
@@ -97,11 +102,13 @@ def execute(C: ry.Config, path: list, grasp_height: float, withHold: bool, onRob
     man = expManip.ManipulationModelling(C)
     man.setup_inverse_kinematics()
     if withHold == True:
-        # man.grasp_top_box(1.,"l_gripper", "hold", "xz")
-        hold_height = C.getFrame("hold").getPosition()[2]
-        man.grasp_top_box_atGivenHeight(1.,"l_gripper", "hold", grasp_height, "xz")
+        hold_height = C.getFrame("hold").getSize()[2]
+        grasp_pos = hold_height/2.0 - grasp_height
+        man.grasp_top_box_atGivenHeight(1.,"l_gripper", "hold", grasp_pos, "xz")
     else:    
-        man.grasp_top_box(1.,"l_gripper", "box", "xz")
+        box_height = C.getFrame("box").getSize()[2]
+        grasp_pos = box_height/2.0 - grasp_height
+        man.grasp_top_box_atGivenHeight(1.,"l_gripper", "box", grasp_pos, "xz")
     pose = man.solve()
 
     if man.feasible:
@@ -109,7 +116,7 @@ def execute(C: ry.Config, path: list, grasp_height: float, withHold: bool, onRob
         robot = robex.Robot(C, onRobot)
         robot.execute_path_blocking(C, pose)
         robot.grasp(C)
-        
+
         # compute path 
         man = expManip.ManipulationModelling(C)
         man.follow_path_on_plane(path, "z", "l_gripper")
@@ -137,14 +144,16 @@ if __name__ == "__main__":
     q_start=[0.12, .12, .0]
     q_goal=[-0.16, .16, .0]
     
-    # scenario, table_height = 'scenarios/pandaSingle.g', 0.65
     scenario, table_height = 'scenarios/pandasTable.g', 0.6
+    # scenario, table_height = 'scenarios/pandaSingle.g', 0.65
 
     box_size = [.07, .075, .06, .001]
+    hold_size = [.04, .04, .05, .001]
+    grasp_height = 0.02
     hold = True
 
 
     # ----- START MAIN -----
 
-    C, path = plan_path(q_pWorld, q_start, q_goal, table_height, scenario, box_size)
-    execute(C, path, 0.015, hold, False)    # True if on real robot
+    C, path = plan_path(q_pWorld, q_start, q_goal, table_height, scenario, box_size, hold_size)
+    execute(C, path, grasp_height, hold, False)    # True if on real robot
